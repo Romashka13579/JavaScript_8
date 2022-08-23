602a14ac59ee5c7ddf7e2188d588163c971c7bc9
Romashka <rroma13579@gmail.com>
2022-08-23 23:34:31 +0000

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var n0
 var calc
+var actions = 0
 var old_number = 0
 var new_number = 0
 var calculation_tab = document.getElementById("calculation-tab")
@@ -10,42 +11,30 @@ function Calcul(type, n) {
         calculation_tab.innerHTML = "" + new_number + ""
     }
     if (type == 2) {
-        // switch (k0) {
-        //     case "plus":
-        //         s0 += s
-        //         calculation_tab.innerHTML = "+"
-        //         console.log(s0);
-        //         s = 0
-        //         break
-        //     case "minus":
-        //         s0 -= s
-        //         calculation_tab.innerHTML = "-"
-        //         console.log(s0);
-        //         s = 0
-        //         break
-        //     case "multiply":
-        //         s0 *= s
-        //         calculation_tab.innerHTML = "*"
-        //         console.log(s0);
-        //         s = 0
-        //         break
-        //     case "divide":
-        //         s0 /= s
-        //         calculation_tab.innerHTML = "/"
-        //         console.log(s0);
-        //         s = 0
-        //         break
-        //     case "equal":
-        //         calculation_tab.innerHTML = "" + s0 + ""
-        //         console.log(s0);
-        //         s0=0
-        //         break
-        //     case "clear":
-        //         s0 = 0
-        //         calculation_tab.innerHTML = ""
-        //         s = 0
-        //         break
-        // }
+        actions ++
+        if(actions > 1 && n != "equal"){
+            switch (n0) {
+                case "plus":
+                    new_number += old_number
+                    console.log(new_number);
+                    break
+                case "minus":
+                    new_number -= old_number
+                    console.log(new_number);
+                    break
+                case "multiply":
+                    new_number *= old_number
+                    console.log(new_number);
+                    break
+                case "divide":
+                    new_number /= old_number
+                    console.log(new_number);
+                    break
+                case "clear":
+                    old_number = 0
+                    break
+            }
+        }
         switch (n) {
             case "plus":
                 old_number = new_number
@@ -75,30 +64,37 @@ function Calcul(type, n) {
                 switch (n0) {
                     case "plus":
                         calc = old_number + new_number
-                        calculation_tab.innerHTML = ""+calc+""
+                        calculation_tab.innerHTML = "" + calc + ""
                         break
                     case "minus":
                         calc = old_number - new_number
-                        calculation_tab.innerHTML = ""+calc+""
+                        calculation_tab.innerHTML = "" + calc + ""
                         break
                     case "multiply":
                         calc = old_number * new_number
-                        calculation_tab.innerHTML = ""+calc+""
+                        calculation_tab.innerHTML = "" + calc + ""
                         break
                     case "divide":
                         calc = old_number / new_number
-                        calculation_tab.innerHTML = ""+calc+""
+                        calculation_tab.innerHTML = "" + calc + ""
                         break
                     case "clear":
-                        calc = 0
-                        calculation_tab.innerHTML = ""+0+""
+                        calc = new_number
+                        calculation_tab.innerHTML = "" + new_number + ""
+                    default:
+                        new_number
                 }
+                new_number = calc
+                actions = 0
                 break
             case "clear":
                 old_number = new_number
                 new_number = 0
                 n0 = n
                 calculation_tab.innerHTML = "clear"
+                actions = 0
+                break
+            default:
                 break
         }
     }
